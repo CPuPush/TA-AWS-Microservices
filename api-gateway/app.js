d910553ba21a5d015b5f93394f8399c6a528fef4
CPuPush <fpakpahan44@gmail.com>
2023-06-14 11:29:57 +0700

    test

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -8,11 +8,12 @@ const cors = require('cors');
 const userRoute = require('./routes/userService');
 const refreshTokenRoute = require('./routes/refreshTokens');
 const medicalRoute = require('./routes/medicalService')
+const mediaRoute = require('./routes/mediaService');
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
@@ -20,5 +21,6 @@ app.use(cors());
 app.use('/user', userRoute);
 app.use('/refresh-token', refreshTokenRoute);
 app.use('/medical-record', medicalRoute);
+app.use('/media', mediaRoute);
 
 module.exports = app;
